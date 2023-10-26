39e034721a877eb620a9001b084de8332501a7fc
Kosuke-Sugimoto <sk12852019@outlook.jp>
2023-10-26 20:44:39 +0900

    can use limitation option

--- make_split_dataset.py
+++ make_split_dataset.py
@@ -2,6 +2,7 @@ import os
 import random
 import shutil
 import argparse
+from tqdm import tqdm
 from omegaconf import OmegaConf
 
 
@@ -11,6 +12,8 @@ if __name__=="__main__":
     parser.add_argument("--base_cfg_path", type=str, default="configs/base_config.yaml")
     parser.add_argument("--dataset_root", type=str, default="dataset/Quickdraw-Dataset/png")
     parser.add_argument("--new_dataset_root", type=str, default="dataset/Quickdraw-Dataset/png_splitted")
+    parser.add_argument("--use_limitation", action="store_true")
+    parser.add_argument("--limitation", type=int, default=1000)
     args = parser.parse_args()
 
     base_cfg = OmegaConf.load(args.base_cfg_path)
@@ -21,7 +24,7 @@ if __name__=="__main__":
     classes = os.listdir(args.dataset_root)
     classes = [_class for _class in classes if _class in base_cfg.target_categories]
 
-    for class_name in classes:
+    for class_name in tqdm(classes):
         class_dir = os.path.join(args.dataset_root, class_name)
         files = os.listdir(class_dir)
         random.shuffle(files)
@@ -30,14 +33,18 @@ if __name__=="__main__":
         train_files = files[:train_size]
         test_files = files[train_size:]
 
+        if args.use_limitation:
+            train_files = [file for i, file in enumerate(train_files) if i<(args.limitation*args.train_ratio) ]
+            test_files = [file for i, file in enumerate(test_files) if i<(args.limitation*(1-args.train_ratio))]
+
         os.makedirs(os.path.join(train_data_dir, class_name), exist_ok=True)
-        for file in train_files:
+        for file in tqdm(train_files, leave=False):
             src = os.path.join(class_dir, file)
             dst = os.path.join(train_data_dir, class_name, file)
             shutil.copy(src, dst)
 
         os.makedirs(os.path.join(test_data_dir, class_name), exist_ok=True)
-        for file in test_files:
+        for file in tqdm(test_files, leave=False):
             src = os.path.join(class_dir, file)
             dst = os.path.join(test_data_dir, class_name, file)
             shutil.copy(src, dst)
